5cb6997177a5b54830dd56b27fc1325c6dd0be70
keenetic29 <al29.andreev@gmail.com>
2024-05-26 16:54:15 +0300

    Update

--- Graduate_Work/Program.cs
+++ Graduate_Work/Program.cs
@@ -14,8 +14,6 @@ namespace Graduate_Work
     [STAThread]
     static void Main()
     {
-      //http://localhost:12345/callback#access_token=y0_AgAAAABsexpOAAtyiwAAAAD-PFE4AABRJVxPHOJCTZOfW7I8lWqk_1XBPg&token_type=bearer&expires_in=31536000
-
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
       Application.Run(new Form1());
